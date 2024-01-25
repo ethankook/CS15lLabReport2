f57b33c75395d31cc71f902dbd98c7d7a7b0e626
ethankook <93116378+ethankook@users.noreply.github.com>
2024-01-25 10:54:40 -0800

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -15,7 +15,7 @@ class Handler implements URLHandler {
         } else {
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
+                if (parameters[0].equals("s")) {
                     num += Integer.parseInt(parameters[1]);
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
